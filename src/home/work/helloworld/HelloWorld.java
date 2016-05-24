5235a02dbd5dc6bbe60f047b796893d946bbc59f
LenaSoldatova <lena.soldatova@gmail.com>
2016-05-24 14:15:02 +0300

    First commit

---
+++ src/home/work/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package home.work.helloworld;
+
+/**
+ * Created by Lena on 17.05.2016.
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
